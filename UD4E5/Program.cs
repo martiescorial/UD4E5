c528eb5c35f64f239092bf4d9759d876305bd9e3
Martina <martinaescorial9@gmail.com>
2021-06-15 21:35:30 +0200

    Agregar archivos de proyecto.

---
+++ UD4E5/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace UD4E5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dia = "martes";
+            switch (dia)
+            {
+
+                case "lunes":
+                case "martes":
+                case "miercoles":
+                case "jueves":
+                case "viernes":
+                    Console.WriteLine("No es fin de semana");
+                    break;
+                case "sabado":
+                case "domingo":
+                    Console.WriteLine("Es fin de semana");
+                    break;
+
+                default:
+                    Console.WriteLine("Ese dia no es correcto");
+                    break;
+            }
+        }
+    }
+}   
